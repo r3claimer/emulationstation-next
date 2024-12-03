06aa13a9197ce51be40080908d61ed1ec4b579ed
spycat88 <spycat88@users.noreply.github.com>
2024-12-03 22:01:52 +0000

    Implement dynamic MaxVRAM

--- es-app/src/ApiSystem.h
+++ es-app/src/ApiSystem.h
@@ -212,6 +212,8 @@ public:
 	virtual std::vector<std::string> getPairedBluetoothDeviceList();	
     virtual bool scanNewBluetooth(const std::function<void(const std::string)>& func = nullptr); // Obsolete
 
+	virtual int GetTotalRam();
+
     std::vector<std::string> getAvailableBackupDevices();
     std::vector<std::string> getAvailableInstallDevices();
     std::vector<std::string> getAvailableInstallArchitectures();
