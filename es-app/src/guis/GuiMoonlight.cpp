65e27be7f79cd3f58ca2b70d5768e0297b04a68b
John Williams <porschemad911@gmail.com>
2025-08-24 23:00:16 +1000

    moonlight - remove profile source from launch scripts

--- es-app/src/guis/GuiMoonlight.cpp
+++ es-app/src/guis/GuiMoonlight.cpp
@@ -229,14 +229,12 @@ bool MoonlightClient::UpdateMoonlightGames() {
     if (fileExists("/storage/.config/Moonlight Game Streaming Project/Moonlight.conf") == true) {
       std::ofstream app_file("/storage/roms/moonlight/" + filename + ".sh");
       app_file << "#!/bin/sh" << std::endl;
-      app_file << ". /etc/profile" << std::endl;
       app_file << "/usr/bin/controller-layout moonlight \"" << filename + ".sh" << "\" /storage/.config/moonlight/gamecontrollerdb.txt" << std::endl;
       app_file << "QT_QPA_PLATFORM=wayland moonlight stream " << server_ip_ << " \"" << title << "\" --quit-after" << std::endl;
       app_file.close();
     } else {
       std::ofstream app_file("/storage/roms/moonlight/" + filename + ".sh");
       app_file << "#!/bin/sh" << std::endl;
-      app_file << ". /etc/profile" << std::endl;
       app_file << "/usr/bin/controller-layout moonlight \"" << filename + ".sh" << "\" /storage/.config/moonlight/gamecontrollerdb.txt" << std::endl;
       app_file << "moonlight stream -app \"" << title << "\" -platform sdl " << server_ip_ << std::endl;
       app_file.close();
