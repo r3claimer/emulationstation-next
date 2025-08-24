b3e5e5edf2a398123895e9dcd0e0c40ce49d9854
John Williams <porschemad911@gmail.com>
2025-08-24 22:49:25 +1000

    moonlight - remove jslisten from launch scripts

--- es-app/src/guis/GuiMoonlight.cpp
+++ es-app/src/guis/GuiMoonlight.cpp
@@ -231,7 +231,6 @@ bool MoonlightClient::UpdateMoonlightGames() {
       app_file << "#!/bin/sh" << std::endl;
       app_file << ". /etc/profile" << std::endl;
       app_file << "/usr/bin/controller-layout moonlight \"" << filename + ".sh" << "\" /storage/.config/moonlight/gamecontrollerdb.txt" << std::endl;
-      app_file << "jslisten set \"moonlight\"" << std::endl;
       app_file << "QT_QPA_PLATFORM=wayland moonlight stream " << server_ip_ << " \"" << title << "\" --quit-after" << std::endl;
       app_file.close();
     } else {
@@ -239,7 +238,6 @@ bool MoonlightClient::UpdateMoonlightGames() {
       app_file << "#!/bin/sh" << std::endl;
       app_file << ". /etc/profile" << std::endl;
       app_file << "/usr/bin/controller-layout moonlight \"" << filename + ".sh" << "\" /storage/.config/moonlight/gamecontrollerdb.txt" << std::endl;
-      app_file << "jslisten set \"moonlight\"" << std::endl;
       app_file << "moonlight stream -app \"" << title << "\" -platform sdl " << server_ip_ << std::endl;
       app_file.close();
     }
